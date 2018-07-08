87a1499b9c46ee9a216802680d272afbbcbc3afb
tinyhazeleyes <6069849+tinyhazeleyes@users.noreply.github.com>
2018-07-08 08:19:57 -0400

    Changed query to only show active tags
    If a tag is deleted the current query will get the tag since when a tag is deleted the tag_count table does not have the tag deleted.  
    
    The tag should be deleted from the tag_count & product_tag tables when a tag is deleted in my opinion.  If you turn the product on and off that had the tag then the tag is deleted, I'm not sure if it has to be the product with the tag.

--- classes/Tag.php
+++ classes/Tag.php
@@ -195,8 +195,8 @@ class TagCore extends ObjectModel
             $groups = FrontController::getCurrentCustomerGroups();
             return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
             SELECT t.name, counter AS times
-            FROM `'._DB_PREFIX_.'tag_count` pt
-            LEFT JOIN `'._DB_PREFIX_.'tag` t ON (t.id_tag = pt.id_tag)
+            FROM `'._DB_PREFIX_.'tag` t
+            LEFT JOIN `'._DB_PREFIX_.'tag_count` pt ON (t.id_tag = pt.id_tag)
             WHERE pt.`id_group` '.(count($groups) ? 'IN ('.implode(',', $groups).')' : '= 1').'
             AND pt.`id_lang` = '.(int) $idLang.' AND pt.`id_shop` = '.(int) $context->shop->id.'
             ORDER BY times DESC
@@ -204,8 +204,8 @@ class TagCore extends ObjectModel
         } else {
             return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
             SELECT t.name, counter AS times
-            FROM `'._DB_PREFIX_.'tag_count` pt
-            LEFT JOIN `'._DB_PREFIX_.'tag` t ON (t.id_tag = pt.id_tag)
+            FROM `'._DB_PREFIX_.'tag` t
+            LEFT JOIN `'._DB_PREFIX_.'tag_count` pt ON (t.id_tag = pt.id_tag)
             WHERE pt.id_group = 0 AND pt.`id_lang` = '.(int) $idLang.' AND pt.`id_shop` = '.(int) $context->shop->id.'
             ORDER BY times DESC
             LIMIT '.(int) $nb);
